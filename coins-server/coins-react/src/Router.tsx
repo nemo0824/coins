2077db3234ab929c7e73f0cbcee125b5020947a2
임재원 <nemo0824@naver.com>
2024-10-22 18:37:28 +0900

    loginform

--- coins-server/coins-react/src/Router.tsx
+++ coins-server/coins-react/src/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Routes, Route } from 'react-router-dom'
 import Coins from './routes/Coins'
 import CoinDetail from './components/CoinDetail'
+import Login from './routes/Login'
 
 const Router = () => {
   return (
@@ -10,6 +11,7 @@ const Router = () => {
      <Routes>
         <Route path='/' element={<Coins></Coins>}/>
         <Route path='/coin/:id' element={<CoinDetail></CoinDetail>}/>
+        <Route path='/login' element={<Login></Login>}/>
      </Routes>
     </BrowserRouter>
     
--- coins-server/coins-react/src/routes/Login.tsx
+++ coins-server/coins-react/src/routes/Login.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 export const Login = () => {
   return (
-    <div className='flex w-full h-full justify-center items-center  '>
-        <div className='flex flex-row'>
-            <div className='w-1/2'>
-                <img src='https://images.unsplash.com/photo-1532043772213-09a6cec77cfa?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
-            </div>
-            <div className='flex flex-col w-1/2 justify-center items-center gap-6'>
-             <h1>회원 가입 </h1>
-             <div>구글</div>
-             <div>
-                <img src='/images/kakao_login_medium_wide.png'></img>
-             </div>
-            </div>
-        </div>
-    </div>
+    <section className='flex justify-center items-center h-screen p-5'>
+      <div className='max-w-[500px] bg-slate-100 h-full rounded-md shadow-md'>
+         <div className='p-4'>
+           <h1>로그인</h1>
+         </div>
+         <div>
+           <img src='https://images.unsplash.com/photo-1641197408799-262f1f343cc6?q=80&w=2972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
+         </div>
+         <div className='mt-10'></div>
+         <div className='cursor-pointer flex justify-center items-chttps://images.unsplash.com/photo-1641197408799-262f1f343cc6?q=80&w=2972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Denter'>
+            <img src='/images/kakao_login_medium_wide.png' alt='kakao 로그인'></img>
+         </div>
+      </div>
+    </section>
   )
 }
 
